310488762ca2656d06f661ecc6abab915f20beda
JessicaS161995 <ojitos16@outlook.es>
2023-11-20 12:14:15 -0500

    color neon particulas

--- clases/particula.js
+++ clases/particula.js
@@ -26,10 +26,18 @@ class Particula {
 			this.isAlive = false;
 		}
     }
-    
+
     display() {
-		fill(this.color);
-		stroke(255);
-		square(this.pos.x, this.pos.y, this.side);
-	}
+        // Colores neon
+        let neonColor = color(
+            sin(frameCount * 0.05) * 255,
+            sin(frameCount * 0.05 + PI / 3) * 255,
+            sin(frameCount * 0.05 + (2 * PI) / 3) * 255
+        );
+    
+        fill(neonColor);
+        stroke(255);
+        square(this.pos.x, this.pos.y, this.side);
+    }
+
 }
