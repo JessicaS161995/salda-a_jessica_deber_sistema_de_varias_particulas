9029fd33aa7461c0ed0574cb81d474d00fde293d
JessicaS161995 <ojitos16@outlook.es>
2023-11-20 12:34:48 -0500

    cromatica y bacground

--- clases/particula.js
+++ clases/particula.js
@@ -1,34 +1,39 @@
 class Particula {
-	constructor(_x, _y, _color) {
-		this.pos = createVector(_x, _y);
-		this.lifespan = Math.ceil(random(10, 100));
-		this.isAlive = true;
-		this.side = Math.ceil(random(3, 15));
-		this.color = _color;
-		this.theta = random(TWO_PI); // Ángulo inicial aleatorio
-		this.radius = random(1, 5); // Radio aleatorio
-	}
-
-	update() {
-		// Movimiento en espiral
-		let x = this.radius * cos(this.theta);
-		let y = this.radius * sin(this.theta);
-
-		this.pos.add(createVector(x, y));
-
-		// Incrementamos el ángulo y ajustamos el radio para obtener una espiral
-		this.theta += 0.05;
-		this.radius += 0.02;
-
-		this.lifespan -= 1;
-
-		if (this.lifespan <= 0) {
-			this.isAlive = false;
-		}
+    constructor(_x, _y) {
+      this.pos = createVector(_x, _y);
+      this.lifespan = Math.ceil(random(10, 100));
+      this.isAlive = true;
+      this.side = Math.ceil(random(3, 15));
+  
+      // Colores magenta aleatorios
+      this.color = color(random(150, 255), 0, random(150, 255));
+  
+      this.theta = random(TWO_PI); // Ángulo inicial aleatorio
+      this.radius = random(1, 5); // Radio aleatorio
     }
-	display() {
-		fill(this.color);
-		stroke(255);
-		square(this.pos.x, this.pos.y, this.side);
-	}
-}
+  
+    update() {
+      // Movimiento en espiral
+      let x = this.radius * cos(this.theta);
+      let y = this.radius * sin(this.theta);
+  
+      this.pos.add(createVector(x, y));
+  
+      // Incrementamos el ángulo y ajustamos el radio para obtener una espiral
+      this.theta += 0.05;
+      this.radius += 0.02;
+  
+      this.lifespan -= 1;
+  
+      if (this.lifespan <= 0) {
+        this.isAlive = false;
+      }
+    }
+  
+    display() {
+      fill(this.color);
+      stroke(255);
+      square(this.pos.x, this.pos.y, this.side);
+    }
+  }
+  
